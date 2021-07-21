7e8956db54d4c0491ab41ab88512f88d837df812
kremlevmax <kremlevmax1989@gmail.com>
2021-07-21 14:03:29 -0700

    First commit

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from "redux";
+
+const counterReducer = (state, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  } else if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
